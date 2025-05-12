3ef366893e1b745d08fac17e22a1ea9b620acd91
valesanprime <hfcnzgby1@mail.ru>
2025-05-13 01:06:13 +0300

    Update models.py
    ////

--- main/models.py
+++ main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
 class Bid(models.Model):
     name = models.CharField((""), max_length=50)
     user = models.CharField((""), max_length=50)
+    status = models.CharField((""), max_length=20, default='pending')  # Новое поле
+    rating = models.IntegerField((""), default=0)  # Новое поле
 
     class Meta:
         verbose_name = ("Заявки")
@@ -12,3 +16,8 @@ class Bid(models.Model):
     def __str__(self):
         return self.name
 
+class Comment(models.Model):  # Новая модель
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name='comments')
+    user = models.CharField(max_length=50)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
